893610f257b77cbcec8110821b7e8f2bb303cb00
SonOfMan12316 <mancycarter@gmail.com>
2025-09-10 00:10:59 +0100

    added font, fontSize, border radius and spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,69 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html", 
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        'ch-beige': '#F8F4F0',
-        'ch-lighter-beige': '#98908B',
-        'ch-grey': '#696868',
-        'ch-green': '#277C78',
-        'ch-cyan': '#82C9D7',
-        'ch-navy': '#626070',
-        'ch-yellow': '#F2CDAC',
-        'ch-dark-grey': '#201F24',
-        'ch-light-grey': '#F2F2F2',
-        'ch-lighter-grey': '#B3B3B3',
-        'ch-red': '#C94736',
-        'bg-beige': '#F2F3F7',
-        'ch-purple': '#826CB0',
-        'ch-magenta': '#934F6F',
-        'ch-torquoise': '#597C7C',
-        'ch-brown': '#93674C',
-        'ch-blue': '#3F82B2',
-        'ch-black': '#000000',
-        'ch-modal': '#000000b3',
-        'ch-red': '#C94736',
-        'ch-danger': '#FE0808',
+        'ch-neutral-900': '#02012C',
+        'ch-neutral-800': '#262540',
+        'ch-neutral-700': '#302F4A',
+        'ch-neutral-600': '#3C3B5E',
+        'ch-grey': '#ACACB7',
+        'ch-light-grey': '#D4D3D9',
+        'ch-orange': '#FF820A',
+        'ch-light-blue': '#4658D9',
+        'ch-dark-blue': '#2B1B9C',
+        'ch-white': '#FFFFFF',
+      },
+      borderRadius: {
+        0: '0rem',
+        4: '0.25rem',
+        6: '0.375rem',
+        8: '0.5rem',
+        10: '0.625rem',
+        12: '0.75rem',
+        16: '1rem',
+        20: '1.25rem',
+        24: '1.5rem',
+        full: '9999px',
+      },
+      spacing: {
+        0: '0rem',
+        '025': '0.125rem',
+        '050': '0.25rem',
+        '075': '0.375rem',
+        100: '0.5rem',
+        125: '0.625rem',
+        150: '0.75rem',
+        200: '1rem',
+        250: '1.25rem',
+        300: '1.5rem',
+        400: '2rem',
+        500: '2.5rem',
+        600: '3rem',
+        800: '4rem',
+        1000: '5rem',
+        1200: '6rem',
+        1400: '7rem',
+        1600: '8rem',
+        1800: '8.75rem',
+      },
+      fontSize: {
+        xs: ['0.875rem', { lineHeight: '1.2' }],
+        sm: ['1.125rem', { lineHeight: '1.2' }],
+        base: ['1.25rem', { lineHeight: '1.2' }],
+        lg: ['1.75rem', { lineHeight: '1.2' }],
+        xl: ['2rem', { lineHeight: '1' }],
+        '2xl': ['3.25rem', { lineHeight: '1.2' }],
+        '3xl': ['6rem', { lineHeight: '1' }],
+        '4xl': ['4rem', { lineHeight: '1.2' }],
+        '5xl': ['8rem', { lineHeight: '1.2' }],
       },
       keyframes: {
         sidebarExpand: {
-          '0%': { 'width': '5.5rem' },
-          '100%': { 'width': '14rem' },
+          '0%': { width: '5.5rem' },
+          '100%': { width: '14rem' },
         },
         sidebarCollapse: {
           '0%': { 'min-width': '14rem' },
@@ -52,8 +82,7 @@ module.exports = {
           '100%': { transform: 'translateY(100%)', opacity: 0 },
         },
       },
-      backgroundImage: {
-      },
+      backgroundImage: {},
       animation: {
         'sidebar-expand': 'sidebarExpand 0.5s ease-in-out forwards',
         'sidebar-collapse': 'sidebarCollapse 0.3s ease-in-out forwards',
@@ -62,12 +91,12 @@ module.exports = {
         slideDown: 'slideDown 0.15s ease-in forwards',
       },
       screens: {
-        'xl': '1200px',
+        xl: '1200px',
       },
     },
     fontFamily: {
       dmSans: ['DM Sans', 'ui-sans-serif'],
-      bricolage: ['Bricolage Grotesque', 'ui-sans-serif']
+      bricolage: ['Bricolage Grotesque', 'ui-sans-serif'],
     },
   },
   variants: {
@@ -77,7 +106,12 @@ module.exports = {
   },
   plugins: [
     function ({ addVariant }) {
-      addVariant('hover-focus-active', ['&:hover', '&:focus', '&:active'], 'expanded', '&.expanded');
+      addVariant(
+        'hover-focus-active',
+        ['&:hover', '&:focus', '&:active'],
+        'expanded',
+        '&.expanded'
+      )
     },
   ],
 }
