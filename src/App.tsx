ad439ecca374e4c4c70e761039ac449b4ab518f7
SonOfMan12316 <mancycarter@gmail.com>
2025-09-13 23:53:25 +0100

    kicked off hero translation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Home from './components/pages/Home'
 import './App.css'
 
 const App = () => {
-    return (
-    <>
-    </>
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/components/pages/Home.tsx
@@ -0,0 +1,11 @@
+import Header from '../HomePage/Header'
+
+const Home = () => {
+  return (
+    <div className="w-screen h-screen bg-ch-neutral-900 text-ch-neutral-900">
+      <Header />
+    </div>
+  )
+}
+
+export default Home
