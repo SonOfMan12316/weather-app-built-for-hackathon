ad4241b26c5b8d795c721ccff3477b7fbddd06b1
SonOfMan12316 <mancycarter@gmail.com>
2025-09-21 21:50:27 +0100

    added react-hot-toast for errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 
 import Home from './components/pages/Home'
 import './App.css'
@@ -6,6 +7,16 @@ import './App.css'
 const App = () => {
   return (
     <Router>
+      <Toaster
+        toastOptions={{
+          error: {
+            style: {
+              background: '#ffcccc',
+              color: '#fe0808',
+            },
+          },
+        }}
+      />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
