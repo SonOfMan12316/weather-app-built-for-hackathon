0fbe83580a6f70c49383177b2fa9ca44076a9db1
SonOfMan12316 <mancycarter@gmail.com>
2025-09-23 21:41:15 +0100

    new icons

--- src/components/icons/Cancel.tsx
+++ src/components/icons/Cancel.tsx
@@ -1,8 +1,8 @@
 const Cancel = ({ className = '', ...props }) => {
   return (
     <svg
-      width="18"
-      height="18"
+      width="40"
+      height="40"
       viewBox="0 0 18 18"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
